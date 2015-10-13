96ddb47402b6a25af5575a808f9c3950d95588e3
Alex Tan <alext@outlook.com.au>
2015-10-14 03:53:07 +1100

    Added entities and missing fields in User entity; changed 'Session' entity to 'Activity' to avoid confusion; entities are based on revised ERD (see in Google Drive)

---
+++ src/java/com/helpmobile/dba/WaitingList.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.helpmobile.dba;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Tanman
+ */
+
+@Entity
+@Table
+public class WaitingList implements Serializable {
+    @Id
+    private long id;
+    @OneToMany
+    @JoinColumn(name = "userId")
+    private User user;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+    
+}
