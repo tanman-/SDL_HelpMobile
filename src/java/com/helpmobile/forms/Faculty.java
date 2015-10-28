f19ee0e63968f4e611ceed0099bffaad1887c40c
Alex Tan <alext@outlook.com.au>
2015-10-29 01:00:56 +1100

    Improved enum

--- src/java/com/helpmobile/forms/Faculty.java
+++ src/java/com/helpmobile/forms/Faculty.java
@@ -10,12 +10,22 @@ package com.helpmobile.forms;
  * @author Tanman
  */
 public enum Faculty {
-    BUSINESS,
-    DAB,
-    FASS,
-    FEIT,
-    GSH,
-    HEALTH,
-    LAW,
-    SCIENCE
+    BUSINESS("Business"),
+    DAB("DAB"),
+    FASS("FASS"),
+    FEIT("FEIT"),
+    GSH("GSH"),
+    HEALTH("Health"),
+    LAW("Law"),
+    SCIENCE("Science");
+    
+    private final String label;
+    
+    private Faculty(String label) {
+        this.label = label;
+    }
+    
+    public String getLabel() {
+        return label;
+    }
 }
