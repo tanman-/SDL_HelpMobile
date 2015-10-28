33a5b01f6fccc717696964c691dffe935dd7a08b
Alex Tan <alext@outlook.com.au>
2015-10-29 01:00:35 +1100

    Added enum element getters - only used by combobox

---
+++ src/java/com/helpmobile/forms/Data.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.helpmobile.forms;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author Tanman
+ */
+@ManagedBean
+@ApplicationScoped
+public class Data {
+    
+    public Faculty[] getFacultys() {
+        return Faculty.values();
+    }
+    
+    public DegreeLevel[] getDegreeLevels() {
+        return DegreeLevel.values();
+    }
+    
+    public YearLevel[] getYearLevels() {
+        return YearLevel.values();
+    }
+    
+    public Residency[] getResidencys() {
+        return Residency.values();
+    }
+    
+    public Gender[] getGenders() {
+        return Gender.values();
+    }
+    
+    public Country[] getCountrys() {
+        return Country.values();
+    }
+}
