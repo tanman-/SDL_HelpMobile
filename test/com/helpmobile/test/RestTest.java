4e4ab87cf0c0b2dc18055fb7a6926b233f06702c
terra <terra@localhost>
2015-11-03 01:06:30 +1100

    Added rest class and automatic test case for it. Current test is semi manual, just check output window for expected result, will be automated.

---
+++ test/com/helpmobile/test/RestTest.java
@@ -0,0 +1,50 @@
+package com.helpmobile.test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.helpmobile.rest.RestAccess;
+import java.io.IOException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author terra
+ */
+public class RestTest {
+    
+    public RestTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void connect() throws IOException{
+        RestAccess restAccess = new RestAccess();
+        String response = restAccess.doGetRequest("misc/campus", "");
+        System.out.println(response);
+    }
+    
+}
