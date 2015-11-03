fc4d94610ae205b0e0a33265ae54475cfc65f022
Alex Tan <alext@outlook.com.au>
2015-11-04 09:57:24 +1100

    Clean up

--- src/java/com/helpmobile/rest/RestAccess.java
+++ src/java/com/helpmobile/rest/RestAccess.java
@@ -59,15 +59,10 @@ public class RestAccess {
         in.close();
         conn.disconnect();
         //print result
-        return (String) response.toString();
+        return response.toString();
     }
 
     public String doPostRequest(String request, Map<String, Object> data, String method) throws MalformedURLException, IOException {
-
-
-
-
-
         StringBuilder postData = new StringBuilder();
         for (Map.Entry<String, Object> param : data.entrySet()) {
             if (postData.length() != 0) {
@@ -78,20 +73,20 @@ public class RestAccess {
             postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
         }
 
-                URL path = new URL(ADDRESS + request+"?active=true");
+        URL path = new URL(ADDRESS + request + "?active=true");
         HttpURLConnection conn = (HttpURLConnection) path.openConnection();
         conn.setRequestMethod(method);
         conn.setRequestProperty("Content-Type", "application/json");
-        
- //      byte[] result = 
-//        conn.setRequestProperty("Content-Length", Integer.toString(result.length));
+
+//      byte[] result = 
+//      conn.setRequestProperty("Content-Length", Integer.toString(result.length));
         conn.setRequestProperty("AppKey", KEY);
         conn.setUseCaches(false);
         conn.setDoInput(true);
-   //     conn.setDoOutput(true);
-  //      OutputStream output = conn.getOutputStream();
-   //     output.write(result);
-    //    output.flush();
+//      conn.setDoOutput(true);
+//      OutputStream output = conn.getOutputStream();
+//      output.write(result);
+//      output.flush();
 
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(conn.getInputStream()));
@@ -133,10 +128,13 @@ public class RestAccess {
         map.put("active", true);
         map.put("workshopSetId", id);
         String data = doPostRequest("workshop/search/?", map, METHOD_GET);
-        
+
         System.out.println(data);
         return mapper.readValue(data, WorkshopList.class);
+    }
 
+    public static void createWorkshopBooking(int workshopId, String studentId, int userId) {
+        
     }
 
 }
