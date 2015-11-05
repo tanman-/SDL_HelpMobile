c0c59db64b972e221c30b5b0b1fd09afd464aef1
terra <terra@localhost>
2015-11-06 06:28:21 +1100

    As originally planned a new cache system has been added to deal with many HELPs weirdenesses. It basically magic, it takes all the load from HELPs by running a through scan and dumping data to faster access system. Expect response time of 10ms

---
+++ src/java/com/helpmobile/managed/HelpCache.java
@@ -0,0 +1,160 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.helpmobile.managed;
+
+import com.helpmobile.dba.AccessFacade;
+import com.helpmobile.dba.User;
+import com.helpmobile.rest.Campus;
+import com.helpmobile.rest.RestAccess;
+import com.helpmobile.rest.Workshop;
+import com.helpmobile.rest.WorkshopBooking;
+import com.helpmobile.rest.WorkshopSet;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author terra
+ */
+@Named
+@Startup
+@Singleton
+public class HelpCache {
+
+    @Inject
+    private RestAccess rest;
+
+    @Inject
+    private AccessFacade facade;
+
+    private List<Campus> campusList = new LinkedList<>();
+    private List<WorkshopBooking> bookings = new LinkedList<>();
+    private final Map<Integer, WorkshopSet> workshopSets = new LinkedHashMap<>();
+    private final Map<Integer, Workshop> workshops = new LinkedHashMap<>();
+    private List<User> students;
+
+    @PostConstruct
+    private void synchData() {
+        try {
+            System.out.println("Web Application Start");
+            System.out.println("Synching HELPS Data");
+
+            //bookings = getRest().searchBooking();
+            List<WorkshopSet> tempWorkshopSets = getRest().getWorkshops(true).getWorkshopSets();
+            LinkedList<Workshop> tempWorkshops = new LinkedList<>();
+            tempWorkshops.addAll(getRest().getAllWorkshops());
+            campusList = getRest().getCampuses();
+            students = extractUsers();
+
+            {
+                Iterator<Workshop> workshopIterator = tempWorkshops.iterator();
+                while (workshopIterator.hasNext()) {
+                    Workshop work = workshopIterator.next();
+                    workshops.put(work.getId(), work);
+                }
+            }
+
+            System.out.println("Processing HELP Data");
+            List<Workshop> reduWorkshops = (List<Workshop>) tempWorkshops.clone();
+            Iterator<WorkshopSet> workshopSetIterator = tempWorkshopSets.iterator();
+            while (workshopSetIterator.hasNext()) {
+                WorkshopSet set = workshopSetIterator.next();
+                set.setWorkshops(new LinkedList<Workshop>());
+                getWorkshopSets().put(set.getId(), set);
+                Iterator<Workshop> workshopIterator = reduWorkshops.iterator();
+
+                while (workshopIterator.hasNext()) {
+                    Workshop work = workshopIterator.next();
+                    if (work.getSetId() == set.getId()) {
+                        set.getWorkshops().add(work);
+                        workshopIterator.remove();
+                    }
+                }
+            }
+
+            System.out.println("Synch Finished");
+        } catch (Exception ex) {
+            Logger.getLogger(HelpCache.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public List<User> extractUsers() {
+        EntityManagerFactory factory = Persistence
+                .createEntityManagerFactory("SDP_HelpMobilePU");
+        EntityManager entityManager = factory.createEntityManager();
+        //entityManager.getTransaction().begin();
+        List<User> listPersons = entityManager.createQuery(
+                "SELECT p FROM User p").getResultList();
+        //entityManager.getTransaction().commit();
+        entityManager.close();
+        factory.close();
+        return listPersons;
+    }
+
+    /**
+     * @return the rest
+     */
+    public RestAccess getRest() {
+        return rest;
+    }
+
+    /**
+     * @return the facade
+     */
+    public AccessFacade getFacade() {
+        return facade;
+    }
+
+    /**
+     * @return the campusList
+     */
+    public List<Campus> getCampusList() {
+        return campusList;
+    }
+
+    /**
+     * @return the bookings
+     */
+    public List<WorkshopBooking> getBookings() {
+        return bookings;
+    }
+
+    /**
+     * @return the workshops
+     */
+    public Map<Integer, Workshop> getWorkshops() {
+        return workshops;
+    }
+
+    /**
+     * @return the students
+     */
+    public List<User> getStudents() {
+        return students;
+    }
+
+    /**
+     * @return the workshopSets
+     */
+    public Map<Integer, WorkshopSet> getWorkshopSets() {
+        return workshopSets;
+    }
+}
--- src/java/com/helpmobile/rest/WorkshopBooking.java
+++ src/java/com/helpmobile/rest/WorkshopBooking.java
@@ -5,7 +5,9 @@
  */
 package com.helpmobile.rest;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.helpmobile.dba.User;
 import static java.lang.Integer.parseInt;
 import java.util.Date;
 
@@ -13,41 +15,137 @@ import java.util.Date;
  *
  * @author terra
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class WorkshopBooking {
 
-    int workshopId;
-    String studentId;
-    int userId;
+/*
+        "BookingId": 12,
+          "workshopID": 6,
+          "studentID": "123456",
+          "topic": "Oral Presentation - evening",
+          "description": "The workshop (total of 10 hours) aims to equip you with the skills to prepare for and deliver an effective presentation.",
+          "targetingGroup": "All UTS students",
+          "campusID": 3,
+          "starting": "2012-07-16T18:00:00",
+          "ending": "2012-07-20T20:00:00",
+          "maximum": 50,
+          "cutoff": null,
+          "canceled": null,
+          "attended": null,
+          "WorkShopSetID": 2,
+          "type": "single",
+          "reminder_num": 9999,
+          "reminder_sent": 0,
+          "WorkshopArchived": null,
+          "BookingArchived": null
+    */
+    private User student;
+    @JsonProperty("studentID")
+    private String studentId;
+    @JsonProperty("BookingId")
+    private int bookingId;
+    private Workshop workshop;
+    @JsonProperty("workshopID")
+    private int workshopId;
+    private String canceled;
+    private String attended;
 
-    public WorkshopBooking(int workshopId, String studentId) {
-        this.workshopId = workshopId;
-        this.studentId = studentId;
-        this.userId = parseInt(studentId);
+    /**
+     * @return the student
+     */
+    public User getStudent() {
+        return student;
     }
-    
 
-    public int getUserId() {
-        return userId;
+    /**
+     * @param student the student to set
+     */
+    public void setStudent(User student) {
+        this.student = student;
     }
 
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public int getWorkshopId() {
-        return workshopId;
-    }
-
-    public void setWorkshopId(int workshopId) {
-        this.workshopId = workshopId;
-    }
-
+    /**
+     * @return the studentId
+     */
     public String getStudentId() {
         return studentId;
     }
 
+    /**
+     * @param studentId the studentId to set
+     */
     public void setStudentId(String studentId) {
         this.studentId = studentId;
+    }
+
+    /**
+     * @return the bookingId
+     */
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    /**
+     * @param bookingId the bookingId to set
+     */
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    /**
+     * @return the workshop
+     */
+    public Workshop getWorkshop() {
+        return workshop;
+    }
+
+    /**
+     * @param workshop the workshop to set
+     */
+    public void setWorkshop(Workshop workshop) {
+        this.workshop = workshop;
+    }
+
+    /**
+     * @return the workshopId
+     */
+    public int getWorkshopId() {
+        return workshopId;
+    }
+
+    /**
+     * @param workshopId the workshopId to set
+     */
+    public void setWorkshopId(int workshopId) {
+        this.workshopId = workshopId;
+    }
+
+    /**
+     * @return the canceled
+     */
+    public String getCanceled() {
+        return canceled;
+    }
+
+    /**
+     * @param canceled the canceled to set
+     */
+    public void setCanceled(String canceled) {
+        this.canceled = canceled;
+    }
+
+    /**
+     * @return the attended
+     */
+    public String getAttended() {
+        return attended;
+    }
+
+    /**
+     * @param attended the attended to set
+     */
+    public void setAttended(String attended) {
+        this.attended = attended;
     }
 
     
